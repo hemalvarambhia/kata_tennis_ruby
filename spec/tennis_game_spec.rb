8dc974dc680b50d0d14393f11fb002f20b6d91c4
Hemal <h.n.varambhia@gmail.com>
2016-08-15 16:47:02 +0100

    An initial test list that specs out player 1 winning the game

---
+++ spec/tennis_game_spec.rb
@@ -0,0 +1,17 @@
+describe 'A game of tennis' do
+  context 'when player 1 has 4 points' do
+    context 'and leads the opponent by 2 points' do
+      it 'confirms player 1 as the winner'
+    end
+  
+    context 'and leads the opponent by > 2 points' do
+      it 'confirms player 1 as the winner'
+    end
+  end
+
+  context 'when player 1 has > 4 points' do
+    context 'and leads the opponent by > 2 points' do
+      it 'confirms player 1 as the winner'
+    end
+  end
+end
