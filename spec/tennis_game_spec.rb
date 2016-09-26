f49c36578ad0fef70554b469279e62f4411a3e42
Hemal <h.n.varambhia@gmail.com>
2016-09-26 14:02:57 +0100

    We use the point_scored method to set up these scenarios.

--- spec/tennis_game_spec.rb
+++ spec/tennis_game_spec.rb
@@ -105,7 +105,10 @@ describe 'A game of tennis' do
   describe 'running score' do
     context 'when player 1 has 3 points and player 2 has 0 points' do
       it 'is 40-0' do
-        running_score = TennisGame.new('player 1' => 3, 'player 2' => 0).score
+        game = TennisGame.new('player 1' => 0, 'player 2' => 0)
+        3.times { game.scored_point 'player 1' } 
+
+        running_score = game.score
 
         expect(running_score).to eq '40-0'
       end
@@ -113,7 +116,10 @@ describe 'A game of tennis' do
 
     context 'when player 1 has 0 points and player 2 has 3 points' do
       it 'is 0-40' do
-        running_score = TennisGame.new('player 1' => 0, 'player 2' => 3).score
+        game = TennisGame.new('player 1' => 0, 'player 2' => 0)
+        3.times { game.scored_point 'player 2' }
+
+        running_score = game.score
 
         expect(running_score).to eq '0-40'
       end
@@ -121,8 +127,15 @@ describe 'A game of tennis' do
 
     context 'when players 1 and 2 have 2 points each' do
       it 'is 30-30' do
-        running_score = TennisGame.new('player 1' => 2, 'player 2' => 2).score        
-        expect(running_score).to eq '30-30'
+        game = TennisGame.new('player 1' => 0, 'player 2' => 0)
+        2.times do
+          game.scored_point 'player 1'
+          game.scored_point 'player 2'
+        end
+
+        running_score = game.score 
+
+        expect(running_score).to eq '30-30' 
       end
     end
 
