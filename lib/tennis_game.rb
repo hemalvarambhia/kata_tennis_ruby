62c4a2b1c7c5afedadff8ba319b3b03cff535d95
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-10-05 21:53:19 +0100

    Moved lead calculation and points description to the Player class.

--- lib/tennis_game.rb
+++ lib/tennis_game.rb
@@ -36,7 +36,8 @@ class TennisGame
   end
 
   def won?(winner, opposition)
-    points(winner) >= 4 and lead_between(winner, opposition) >= 2
+    @players[winner].points >= 4 and
+      @players[winner].lead_over(@players[opposition]) >= 2
   end
 
   def player_with_advantage
@@ -45,22 +46,19 @@ class TennisGame
   end
   
   def advantage?(player, opposition)
-    points(player) >= 3 and points(opposition) >= 3 and 
-      lead_between(player, opposition) == 1
+    @players[player].points >= 3 and @players[opposition].points >= 3 and
+      @players[player].lead_over(@players[opposition]) == 1
   end
 
   def deuce?
-    points(@player_1) >= 3 and points(@player_2) >= 3 and
-      lead_between(@player_1, @player_2) == 0
-  end
-
-  def lead_between(player, opposition)
-    points(player) - points(opposition)
+    @players[@player_1].points >= 3 and @players[@player_1].points >=3 and
+      @players[@player_1].lead_over(@players[@player_2]) == 0
   end
 
   def running_score
-    description = { 3 => 40, 2 => 30, 1 => 15, 0 => 0 }
-    "#{description[points(@player_1)]}-#{description[points(@player_2)]}"
+    player_1 = @players[@player_1]
+    player_2 = @players[@player_2]
+    "#{player_1.points_description}-#{player_2.points_description}"
   end
 
   class Player
@@ -73,6 +71,15 @@ class TennisGame
 
     def scored_point
       @points += 1
+    end
+
+    def lead_over opponent
+      points - opponent.points
+    end
+
+    def points_description
+     description = { 3 => '40', 2 => '30', 1 => '15', 0 => '0' }
+     description[@points]
     end
   end
 end
